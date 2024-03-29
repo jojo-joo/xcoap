a8d5f401883992a854e7ff70b4a4d6fab24a3cb4
jojo-joo <3759526109@qq.com>
2024-03-29 17:26:01 +0800

    vue3 + element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus' //全局引入
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
---
+++ src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <el-input class="input" v-model="input" type="file" placeholder="Please input" />
+        <el-button class="button" type="primary">文件处理</el-button>
+        <el-button type="primary" :icon="Edit" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Delete, Edit, Search, Share, Upload } from '@element-plus/icons-vue'
+import { ElButton, ElInput } from 'element-plus'
+</script>
+
+<style scoped>
+.input {
+    display: inline;
+    margin: 20px 30px;
+}
+
+.button {
+    width: 90px;
+}
+</style> 
